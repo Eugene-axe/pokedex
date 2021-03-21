de505919a077cc47f20322b9505bd2b0d8fc1cff
Eugene Axe <siniypetr@gmail.com>
2021-03-21 14:56:25 +0300

    finish

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,44 +1,105 @@
-import React from 'react';
-import { AppBar, Card , CardContent , CardMedia , Grid,  Toolbar } from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
+import React, { useEffect, useState } from 'react';
+import {toFirstCharUppercase} from './constants'
+import { AppBar, Card , CardContent , CardMedia, CircularProgress, Grid,  TextField,  Toolbar, Typography } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search'
+import {fade ,makeStyles} from '@material-ui/core/styles';
+import axios from 'axios'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     pokedexContainer : {
         paddingTop: 20,
         paddingLeft: 50,
         paddingRight: 50,
+    },
+    cardMedia : {
+        margin: 'auto',
+    },
+    searchContainer: {
+        display : 'flex',
+        backgroundColor: fade(theme.palette.common.white , 0.15),
+        paddingLeft: 20,
+        paddingright: 20,
+        marginTop: 5,
+        marginBottom: 5,
+    },
+    searchIcon : {
+        alignSelf: 'flex-end',
+        marginBottom: 5,
     }
-})
+}));
 
-const getPokemonCard = () => {
+const getPokemonCard = ({pokemonId, pokemonData , classes , history}) => {
+    const {id , name , sprite} = pokemonData[pokemonId];
     return (
-        <Grid item xs={12} sm={4}>
-            <Card>
-                <CardMedia>
-
-                </CardMedia>
+        <Grid item xs={12} sm={4} key={pokemonId}>
+            <Card onClick= {() => history.push(`./${id}`)}>
+                <CardMedia
+                    className={classes.cardMedia}
+                    image={sprite}
+                    style = {{width : 130 , height: 130}}    
+                 />
                 <CardContent>
-                    Hi
+                    <Typography>{`${id}. ${toFirstCharUppercase(name)}`}</Typography>
                 </CardContent>
             </Card>
         </Grid>
     )
 }
 
-const Pokedex = () => {
-    const classes = useStyles()
+const Pokedex = props => {
+    console.log(props);
+    const { history } = props;
+    const classes = useStyles();
+    const [pokemonData , setPokemonData] = useState();
+    const [filter , setFilter] = useState('');
+
+    const handleSearchChange = event => {
+        setFilter(event.target.value);
+    }
+
+    useEffect(()=>{
+        axios.get(`https://pokeapi.co/api/v2/pokemon?limit=50`)
+        .then(response => {
+            console.log('response' , response)
+            const { data } = response;
+            const { results } = data;
+            const newPokemonData = {};
+            results.forEach( (pokemon , index) => {
+                newPokemonData[index+1] = {
+                    id : index + 1 ,
+                    name : pokemon.name,
+                    sprite : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`
+                }
+            })
+            setPokemonData(newPokemonData);
+        })
+    } , [])
     return (
         <>
-            <AppBar position='static'>
-                <Toolbar />
-            </AppBar>
-            <Grid container spacing={2} className={classes.pokedexContainer}>
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-            </Grid>
+        <AppBar position='static'>
+            <Toolbar>
+                <div className={classes.searchContainer}>
+                    <SearchIcon className={classes.searchIcon} />
+                    <TextField
+                        onChange={handleSearchChange} 
+                        className={classes.searchTextField} 
+                        label='Pokemon' 
+                        variant='standard' 
+                    />
+                </div>
+            </Toolbar>
+        </AppBar>
+            {pokemonData ? (
+                <Grid container spacing={2} className={classes.pokedexContainer}>
+                    {Object.keys(pokemonData).map(pokemonId =>
+                        pokemonData[pokemonId].name.includes(filter) &&
+                        getPokemonCard({pokemonId , pokemonData ,classes , history}))
+                    }
+                </Grid>
+            ) : (
+                <CircularProgress />
+            )}
+            
         </>
     )
 }
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import { Button, CircularProgress, Link, Typography } from '@material-ui/core';
+import React , {useEffect, useState} from 'react';
+import {toFirstCharUppercase} from './constants'
+import axios from 'axios'
+
+
 
 const Pokemon = props => {
-    const {match} = props;
+    const {match , history} = props;
     const {params} = match;
     const {pokemonId} = params;
-    console.log(props);
-    console.log(params);
-    console.log(pokemonId);
+    const [pokemon , setPokemon] = useState(undefined)
+
+    useEffect( ()=> {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+        .then(response => {
+            const {data} = response;
+            setPokemon(data);
+        })
+        .catch( error => setPokemon(false))
+    }, [pokemonId])
+
+    const generatePokemonJSX = () => {
+        const { name , id , species , height , weight , types , sprites} = pokemon;
+        const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`
+        const {front_default} = sprites;
+        return (
+            <>
+                <Typography variant='h1'>
+                    {`${id}.`} {toFirstCharUppercase(name)}
+                    <img src={front_default}/>
+                </Typography>
+                <img src={fullImageUrl} alt={name} style={{width: 300 , height: 300}}/>
+                <Typography variant='h3'>Pokemon INFO</Typography>
+                <Typography>
+                    Species: 
+                    <Link href={species.url}>{species.name}</Link>
+                </Typography>
+                <Typography>Height : {height}</Typography>
+                <Typography>Weight : {weight}</Typography>
+                <Typography variant='h6'>Types : </Typography>
+                {types.map((typeInfo) => {
+                    const {type} = typeInfo;
+                    const {name} = type;
+                    return <Typography key={name}>{name}</Typography>
+                })}
+            </>
+        )
+    }
+
     return (
-        <div>{`This is Pokemon page for pokemon #${pokemonId}`}</div>
+        <>
+            {pokemon === undefined && <CircularProgress /> }
+            {pokemon !== undefined && pokemon && generatePokemonJSX() }
+            {pokemon === false && <Typography>Pokemon not found</Typography> }
+            <Button variant='contained' onClick={()=> history.push('/')}>back to pokedex</Button>
+        </>
     )
 }
 
